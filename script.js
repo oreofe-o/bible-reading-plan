956873682442593b1f6973f4e9eb5683789d2d3a
Oreofe <67136829+oreofe-o@users.noreply.github.com>
2026-02-04 23:37:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChang
 
 const firebaseConfig = {
     apiKey: "AIzaSyBeJCtncp2ePYPwdQjWmD8tognJIiUnl40",
-    authDomain: "otf-bible-2026.firebaseapp.com",
+    // authDomain: "otf-bible-2026.firebaseapp.com",
+    authDomain: "luxury-gaufre-48f987.netlify.app",
     projectId: "otf-bible-2026",
     storageBucket: "otf-bible-2026.firebasestorage.app",
     messagingSenderId: "764497402440",
